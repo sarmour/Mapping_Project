c04937275fe7e1a4ba95827c6344abacff2aaef6
Shane Armour <armour.shane@gmail.com>
2015-06-10 11:08:54 -0700

    New out maps.

--- PATpy/CodeTester.py
+++ PATpy/CodeTester.py
@@ -1,49 +1,45 @@
 import imp
 import arcpy
+import os
 PATpy = imp.load_source('PATpy', 'C:\Mapping_Project\PATpy\JoinFunctions.py')
 
 
+#### Set Up Workspace ###
 
+# Patpy.CreateDirs()
+# workspace = Patpy.CreateWorkspace
+# Patpy.SetupPerilsEUWS('CRESTA')
+#SetupPerilsEUWS('POST')
+
+#########################
 workspace = "C:\Mapping_Project\workspace.gdb"
-
-shpfile =  "C:\Mapping_Project\Shapefiles\RL13EUWS_PostCresta.shp"
-csvfile =  "C:\Mapping_Project\TestPC.csv"
+shpfile =  "C:\Mapping_Project\Shapefiles\EUWS2011_CRESTAwJoinCol.shp"
+csvfile =  "C:\Mapping_Project\TestCRESTA.csv"
 
 # shpfile =  "C:\Mapping_Project\Shapefiles\EUWS2011_CRESTAwJoinCol.shp"
 # csvfile =  "C:\Mapping_Project\TestCRESTA.csv"
 
-## first step is to create the directory on the local c drive
-#PATpy.CreateDirs()
-#PATpy.SetupPerilsEUWS('CRESTA')
-
-
 # csvcols = PATpy.GetCSVcols(csvfile)
 # print csvcols
 
+mappingcols = ['Haz','Vuln','Overall']
 
-
-
-mappingcols = ['GU_Haz','GU_Vuln','GU_Overall']
-# PATpy.RemoveSHPcols(shpfile, mappingcols)
-
-# # PATpy.RemoveSHPcols(shpfile, mappingcols)
-# # shpcols = PATpy.GetSHPcols(shpfile)
-# # print shpcols
+# shpcols = PATpy.GetSHPcols(shpfile)
+# print shpcols
 
 # jointable = PATpy.JoinCSV(csvfile, workspace)
-# PATpy.JoinSHP(jointable,"Joincol",shpfile,'JOIN',mappingcols) #this process can take a while
-# print PATpy.CheckMissingSHPVals(csvfile,0, shpfile, 'JOIN')
+# PATpy.JoinSHP(jointable,"POSTCODE",shpfile,'RMS_CRESTA',mappingcols) #this process can take a while
+#print PATpy.CheckMissingSHPVals(csvfile,0, shpfile, 'RMS_CRESTA')
 
 
 
-mxdlist = PATpy.GetMXDList()[1]
+mxdlist = PATpy.GetMXDList()[0]
 print mxdlist
 
-# # lyrs = PATpy.GetLayers(mxdlist)
-# # for lyr in lyrs:
-# #     print lyr
-# # lyr = 'EUWS2011_CRESTAwJoinCol'
+PATpy.GetLayers(mxdlist)
 
-lyr = "RL13EUWS_PostCresta"
-PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Diff_LC")
-# #PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Percent_Change")
+lyr = os.path.basename(shpfile).strip(".shp")
+# print lyr
+
+##PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Diff_LC")
+PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Percent_Change")
