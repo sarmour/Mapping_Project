094a31ba8180b2eee4d7403f2ca087cfde444eda
Shane Armour <armour.shane@gmail.com>
2015-06-09 18:01:19 -0700

    Adding new diff LC join functions. Also testing postcode output.

--- PATpy/CodeTester.py
+++ PATpy/CodeTester.py
@@ -1,36 +1,49 @@
 import imp
 import arcpy
 PATpy = imp.load_source('PATpy', 'C:\Mapping_Project\PATpy\JoinFunctions.py')
+
+
+
+workspace = "C:\Mapping_Project\workspace.gdb"
+
 shpfile =  "C:\Mapping_Project\Shapefiles\RL13EUWS_PostCresta.shp"
 csvfile =  "C:\Mapping_Project\TestPC.csv"
-workspace = "C:\Mapping_Project\workspace.gdb"
+
+# shpfile =  "C:\Mapping_Project\Shapefiles\EUWS2011_CRESTAwJoinCol.shp"
+# csvfile =  "C:\Mapping_Project\TestCRESTA.csv"
 
 ## first step is to create the directory on the local c drive
 #PATpy.CreateDirs()
 #PATpy.SetupPerilsEUWS('CRESTA')
 
 
-# shpcols = PATpy.GetSHPcols(shpfile)
-# print shpcols
 # csvcols = PATpy.GetCSVcols(csvfile)
 # print csvcols
 
 
-# PATpy.RemoveSHPcols(shpfile, mappingcols)
+
 
 mappingcols = ['GU_Haz','GU_Vuln','GU_Overall']
-# jointable = PATpy.JoinCSV(csvfile, workspace)
-# PATpy.JoinSHP(jointable,"Joincol",shpfile,'JOIN',mappingcols)
-# print PATpy.CheckMissingSHPVals(csvfile,0, shpfile, 'JOIN')
-
 # PATpy.RemoveSHPcols(shpfile, mappingcols)
 
-mxdlist = PATpy.GetMXDList()[1]
+# # PATpy.RemoveSHPcols(shpfile, mappingcols)
+# # shpcols = PATpy.GetSHPcols(shpfile)
+# # print shpcols
 
-lyrs = PATpy.GetLayers(mxdlist)
-for lyr in lyrs:
-    print lyr
-# lyr = 'EUWS2011_CRESTAwJoinCol'
+# jointable = PATpy.JoinCSV(csvfile, workspace)
+# PATpy.JoinSHP(jointable,"Joincol",shpfile,'JOIN',mappingcols) #this process can take a while
+# print PATpy.CheckMissingSHPVals(csvfile,0, shpfile, 'JOIN')
+
+
+
+mxdlist = PATpy.GetMXDList()[1]
+print mxdlist
+
+# # lyrs = PATpy.GetLayers(mxdlist)
+# # for lyr in lyrs:
+# #     print lyr
+# # lyr = 'EUWS2011_CRESTAwJoinCol'
 
 lyr = "RL13EUWS_PostCresta"
-PATpy.CreateMapDiffLC(mxdlist,lyr,mappingcols)
+PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Diff_LC")
+# #PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Percent_Change")
--- PATpy/deletme.py
+++ PATpy/deletme.py
@@ -1,14 +1,36 @@
-mxds = 'test.mxd'
-mapfields = 'test.mxd'
-print mxds
-print mapfields
-if type(mxds) is str:
-    newmxd = []
-    newmxd.append(mxds)
-    mxds = newmxd
-if type(mapfields) is str:
-    newmapfields = []
-    newmapfields.append(mapfields)
-    mapfields = newmapfields
-print mxds
-print mapfields
+# mxds = 'test.mxd'
+# mapfields = 'test.mxd'
+# print mxds
+# print mapfields
+# if type(mxds) is str:
+#     newmxd = []
+#     newmxd.append(mxds)
+#     mxds = newmxd
+# if type(mapfields) is str:
+#     newmapfields = []
+#     newmapfields.append(mapfields)
+#     mapfields = newmapfields
+# print mxds
+# print mapfields
+
+import sys
+import csv
+import operator
+
+csvfile =  "C:\Mapping_Project\TestPC.csv"
+reader = csv.reader(open(csvfile), delimiter=",")
+i = 0
+for l in reader:
+    i +=1
+    if i <10 :
+        print l
+    else:
+        break
+sortedlist = sorted(reader, key=operator.itemgetter(0))
+
+# with open(csvfile) as csvfile:
+#     for l in csvfile:
+#         print l
+for l in sortedlist[:10]:
+    print l
+
