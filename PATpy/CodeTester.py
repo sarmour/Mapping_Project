d7a041cb0682a1abd2f93386f7c0cf1a30bc8199
Roopa Nair <=nair.roopa@gmail.com>
2015-06-10 14:40:26 -0700

    Forked repo. Updated set-up functions.

--- PATpy/CodeTester.py
+++ PATpy/CodeTester.py
@@ -6,18 +6,15 @@ PATpy = imp.load_source('PATpy', 'C:\Mapping_Project\PATpy\JoinFunctions.py')
 
 #### Set Up Workspace ###
 
-# Patpy.CreateDirs()
-# workspace = Patpy.CreateWorkspace
-# Patpy.SetupPerilsEUWS('CRESTA')
+#PATpy.CreateDirs()
+#workspace = PATpy.CreateWorkspace()
+##PATpy.SetupPerilsEUWS('CRESTA')
 #SetupPerilsEUWS('POST')
 
-#########################
+# #########################
 workspace = "C:\Mapping_Project\workspace.gdb"
 shpfile =  "C:\Mapping_Project\Shapefiles\EUWS2011_CRESTAwJoinCol.shp"
 csvfile =  "C:\Mapping_Project\TestCRESTA.csv"
-
-# shpfile =  "C:\Mapping_Project\Shapefiles\EUWS2011_CRESTAwJoinCol.shp"
-# csvfile =  "C:\Mapping_Project\TestCRESTA.csv"
 
 # csvcols = PATpy.GetCSVcols(csvfile)
 # print csvcols
@@ -25,21 +22,22 @@ csvfile =  "C:\Mapping_Project\TestCRESTA.csv"
 mappingcols = ['Haz','Vuln','Overall']
 
 # shpcols = PATpy.GetSHPcols(shpfile)
+
 # print shpcols
 
 # jointable = PATpy.JoinCSV(csvfile, workspace)
 # PATpy.JoinSHP(jointable,"POSTCODE",shpfile,'RMS_CRESTA',mappingcols) #this process can take a while
-#print PATpy.CheckMissingSHPVals(csvfile,0, shpfile, 'RMS_CRESTA')
+# print PATpy.CheckMissingSHPVals(csvfile,0, shpfile, 'RMS_CRESTA')
 
 
 
 mxdlist = PATpy.GetMXDList()[0]
 print mxdlist
 
-PATpy.GetLayers(mxdlist)
+# PATpy.GetLayers(mxdlist)
 
 lyr = os.path.basename(shpfile).strip(".shp")
-# print lyr
+print lyr
 
-##PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Diff_LC")
+# ##PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Diff_LC")
 PATpy.CreateMaps(mxdlist,lyr,mappingcols,"Percent_Change")
